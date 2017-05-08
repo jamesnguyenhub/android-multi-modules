5b058a0eafc8401f5322c4c41383a7e562cf698e
Tuyen Nguyen <nntuyen92@gmail.com>
2017-05-08 14:12:13 +0700

    Init some bases

--- base/src/main/java/com/tuyenmonkey/base/BaseActivity.java
+++ base/src/main/java/com/tuyenmonkey/base/BaseActivity.java
@@ -1,10 +1,17 @@
 package com.tuyenmonkey.base;
 
+import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
+import butterknife.ButterKnife;
 
 /**
  * Created by Tuyen Monkey on 5/8/17.
  */
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+  @Override public void setContentView(@LayoutRes int layoutResID) {
+    super.setContentView(layoutResID);
+    ButterKnife.bind(this);
+  }
 }
