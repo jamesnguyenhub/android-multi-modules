f6fc0a96740c7e35a9ef21d3ff5717abbca87341
Tuyen Nguyen <nntuyen92@gmail.com>
2017-05-08 10:30:23 +0700

    Config gradle

--- data/src/main/java/com/tuyenmonkey/data/entity/GitHubRepository.java
+++ data/src/main/java/com/tuyenmonkey/data/entity/GitHubRepository.java
@@ -1,14 +1,16 @@
 package com.tuyenmonkey.data.entity;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Tuyen Monkey on 5/8/17.
  */
 
 public class GitHubRepository {
-  private long id;
-  private String name;
-  private String fullName;
-  private User user;
-  private boolean privateAccess;
-  private String htmlUrl;
+  @SerializedName("id") private long id;
+  @SerializedName("name") private String name;
+  @SerializedName("full_name") private String fullName;
+  @SerializedName("user") private User user;
+  @SerializedName("private") private boolean privateAccess;
+  @SerializedName("html_url") private String htmlUrl;
 }
--- data/src/main/java/com/tuyenmonkey/data/entity/User.java
+++ data/src/main/java/com/tuyenmonkey/data/entity/User.java
@@ -1,15 +1,17 @@
 package com.tuyenmonkey.data.entity;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Tuyen Monkey on 5/8/17.
  */
 
 public class User {
-  private long id;
-  private String login;
-  private String avatarUrl;
-  private String gravatarId;
-  private String url;
-  private String htmlUrl;
-  private String followersUrl;
+  @SerializedName("id") private long id;
+  @SerializedName("login") private String login;
+  @SerializedName("avatar") private String avatarUrl;
+  @SerializedName("gravatar_id") private String gravatarId;
+  @SerializedName("id") private String url;
+  @SerializedName("html_url") private String htmlUrl;
+  @SerializedName("followers_url") private String followersUrl;
 }
