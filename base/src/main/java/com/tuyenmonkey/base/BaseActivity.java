433410b5a8bb33ba6fa6a42811ee4727f44e6979
Tuyen Nguyen <nntuyen92@gmail.com>
2017-05-08 10:57:36 +0700

    Config gradle

---
+++ base/src/main/java/com/tuyenmonkey/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.tuyenmonkey.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tuyen Monkey on 5/8/17.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+}
---
+++ base/src/main/java/com/tuyenmonkey/base/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.tuyenmonkey.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Tuyen Monkey on 5/8/17.
+ */
+
+public abstract class BaseFragment extends Fragment {
+}
