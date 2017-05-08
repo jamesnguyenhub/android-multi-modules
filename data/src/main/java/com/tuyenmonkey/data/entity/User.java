de304232063b16c2fa04bc0de74c0c2d01a9cc81
Tuyen Nguyen <nntuyen92@gmail.com>
2017-05-08 09:11:55 +0700

    Add data module

---
+++ data/src/main/java/com/tuyenmonkey/data/entity/User.java
@@ -0,0 +1,15 @@
+package com.tuyenmonkey.data.entity;
+
+/**
+ * Created by Tuyen Monkey on 5/8/17.
+ */
+
+public class User {
+  private long id;
+  private String login;
+  private String avatarUrl;
+  private String gravatarId;
+  private String url;
+  private String htmlUrl;
+  private String followersUrl;
+}
