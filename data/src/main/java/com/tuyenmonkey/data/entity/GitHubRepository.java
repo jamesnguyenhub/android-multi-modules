ccf3ae6fa0b9d755f51a8e0a1d9de5f6700ae489
Tuyen Nguyen <nntuyen92@gmail.com>
2017-05-08 09:23:18 +0700

    Add githubrepository

---
+++ data/src/main/java/com/tuyenmonkey/data/entity/GitHubRepository.java
@@ -0,0 +1,14 @@
+package com.tuyenmonkey.data.entity;
+
+/**
+ * Created by Tuyen Monkey on 5/8/17.
+ */
+
+public class GitHubRepository {
+  private long id;
+  private String name;
+  private String fullName;
+  private User user;
+  private boolean privateAccess;
+  private String htmlUrl;
+}
